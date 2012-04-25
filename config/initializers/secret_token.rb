f715b9b20e3bbc365c2e8c9dd2c74b6108d822c3
John Whitley <john@luminous-studios.com>
2012-04-25 12:10:40 -0700

    New Rails 3.2.3 template

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RequirejsRailsJasmineTemplate::Application.config.secret_token = '1c92b4125d6f960573aadeeda10d6903740e8dea9bfe2d7ed4f8ca90dfde4d4382c4c654a8318b6956759d3632cdb48dd805308d7a14c8cbdebfed9b3c751ed5'
